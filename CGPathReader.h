b833e44987e8bc4f61960942e93cc54e7e154569
Marcus Rohrmoser <mrohrmoser@acm.org>
2010-11-13 16:52:26 +0100

    Add a C-Wrapper for svg path parsing

---
+++ CGPathReader.h
@@ -0,0 +1,19 @@
+/*
+ *  CGPathReader.h
+ *
+ *  Created by Marcus Rohrmoser on 13.11.10.
+ *  Copyright 2010 Marcus Rohrmoser mobile Software. All rights reserved.
+ *
+ */
+
+#include <CoreGraphics/CGPath.h>
+#include <CoreFoundation/CFError.h>
+
+/** Parse the svg path and return a retained CGPathRef.
+ * Currently just a C-wrapper around PathParser::parseChar:length:trafo:error:
+ * but might become plain C some day.
+ * @param path the svg <path d="..."> data
+ * @param error pointer to a CFError/NSError object
+ * @return a retained CGPathRef
+ */
+CGPathRef CGPathCreateFromSVG(const char *path, CFErrorRef *error);
---
+++ CGPathReader.m
@@ -0,0 +1,21 @@
+//
+//  CGPathReader.c
+//
+//  Created by Marcus Rohrmoser on 11.03.10.
+//  Copyright 2010 Marcus Rohrmoser mobile Software. All rights reserved.
+//
+
+#import "CGPathReader.h"
+#import "PathParser.h"
+
+CGPathRef CGPathCreateFromSVG(const char *path, CFErrorRef *errPtr)
+{
+	if ( path == NULL )
+		return NULL;
+	const size_t len = strlen(path);
+	// #FIXME check overflow
+	PathParser *p = [alloc (PathParser)init];
+	CGPathRef ret = [p parseChar:path length:len trafo:NULL error:(NSError **)errPtr];
+	[p release];
+	return ret;
+}
