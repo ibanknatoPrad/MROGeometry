08eabd8381f48e887b7910a1de270944bd9d130e
Marcus Rohrmoser <mrohrmoser@acm.org>
2014-03-21 23:13:52 +0100

    add a license.

--- MROGeometry/CGPathWriter.c
+++ MROGeometry/CGPathWriter.c
@@ -1,8 +1,28 @@
 /*
- *  CGPathWriter.c
+ * CGPathWriter.c
  *
- *  Created by Marcus Rohrmoser on 15.10.10.
- *  Copyright 2010-2014 Marcus Rohrmoser mobile Software. All rights reserved.
+ * Created by Marcus Rohrmoser on 15.10.10.
+ *
+ * Copyright (c) 2010-2014, Marcus Rohrmoser mobile Software
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. The software must not be used for military or intelligence or related purposes nor
+ * anything that's in conflict with human rights as declared in http://www.un.org/en/documents/udhr/ .
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
@@ -24,7 +44,7 @@
 #define MRLogD(x, ...)
 #endif
 
-// / internal, temporary helper struct
+/** internal, temporary helper struct */
 struct CGPathWriter_t {
 	char *buffer;
 	bool absolute;
@@ -32,10 +52,10 @@ struct CGPathWriter_t {
 	size_t used;
 	size_t increment;
 };
-// / internal, temporary helper struct
+/** internal, temporary helper struct */
 typedef struct CGPathWriter_t CGPathWriter_t;
 
-// / internal, re-allocating snprintf helper
+/** internal, re-allocating snprintf helper */
 void CGPathWriter_snprintf(CGPathWriter_t *const t, char *fmt, ...)
 {
 	assert(t != NULL);
@@ -70,7 +90,7 @@ void CGPathWriter_snprintf(CGPathWriter_t *const t, char *fmt, ...)
 }
 
 
-// / internal Helper
+/** internal Helper */
 void CGPathWriter_path_walker(void *info, const CGPathElement *elm)
 {
 	assert(info != NULL);
